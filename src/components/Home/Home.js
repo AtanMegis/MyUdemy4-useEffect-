809b135f264a97e963c8cc96c9a5b3d8fd74ebd7
megis <atanmegis.cn@gmail.com>
2022-05-23 13:59:33 +0700

    FFINISIHEDD

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
+import Button from '../UI/Button/Button';
+import AuthContext from '../store/Auth-context';
 
-const Home = (props) => {
+const Home = () => {
+  const authCtx = useContext(AuthContext)
+  
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
-      <button onClick={props.onLogout}>Log Out</button>
+      <Button onClick={authCtx.onLogout}>Log Out</Button>
     </Card>
   );
 };
--- src/components/store/auth-context.js
+++ src/components/store/auth-context.js
@@ -1,7 +1,43 @@
-import react from "react";
+import react, { useState, useEffect } from "react";
 
 const AuthContext = react.createContext({
-    isLoggedIn : false
+    isLoggedIn: false,
+    onLogout: () => { },
+    onLogin: (email, password) => { },
 })
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    useEffect(() => {
+        const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+        if (storedUserLoggedInInformation === '1') {
+          setIsLoggedIn(true);
+        }
+      }, []);
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn')
+        setIsLoggedIn(false)
+    }
+
+    const loginHandler = () => {
+        localStorage.setItem('isLoggedIn', '1') // 1 = sucess Login || 0 = gaggal login)
+        setIsLoggedIn(true)
+    }
+
+    return (
+        <AuthContext.Provider
+            value={
+                {
+                    isLoggedIn: isLoggedIn,
+                    onLogout: logoutHandler,
+                    onLogin: loginHandler
+                }}
+        >
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
 
 export default AuthContext;
